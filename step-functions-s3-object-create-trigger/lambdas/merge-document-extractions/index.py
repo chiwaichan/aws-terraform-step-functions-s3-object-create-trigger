46bdc479c6df40f7894828d3a623cb118d101c68
Chiwai Chan <chiwaichan@Chiwais-MacBook-Pro.local>
2024-08-02 22:39:21 +1200

    comment

--- step-functions-s3-object-create-trigger/lambdas/merge-document-extractions/index.py
+++ step-functions-s3-object-create-trigger/lambdas/merge-document-extractions/index.py
@@ -5,6 +5,11 @@ def lambda_handler(events, context):
 
     # Iterate over the rest of the array
     for event in events:
+        if "textract-csv" in event:
+            result['textract-csv'] = event["textract-csv"] 
+        if "bedrock-csv" in event:
+            result['bedrock-csv'] = event["bedrock-csv"] 
+            
         for sub_processing_note in event["sub_processing_notes"]:
             result["processing_notes"].append(sub_processing_note)
 
