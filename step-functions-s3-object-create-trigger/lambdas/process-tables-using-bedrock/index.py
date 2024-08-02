8ec37b865afb940fb4a664597270d21a1af2df0e
Chiwai Chan <chiwaichan@Chiwais-MacBook-Pro.local>
2024-08-02 20:33:55 +1200

    comment

--- step-functions-s3-object-create-trigger/lambdas/process-tables-using-bedrock/index.py
+++ step-functions-s3-object-create-trigger/lambdas/process-tables-using-bedrock/index.py
@@ -1,9 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # This function does nothing
-    return {
-        'statusCode': 200,
-        'body': json.dumps('This Lambda function does nothing.')
-    }
+    event["sub_processing_notes"] = ['Extracted using Bedrock']
+
+    return event
 
